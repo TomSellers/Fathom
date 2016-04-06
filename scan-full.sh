ae8ee09fbad4fd815742f130ef6cd07570c7d235
Tom Sellers <tom@fadedcode.net>
2016-04-06 08:04:07 -0500

    Add 88/udp and 389/udp to scan-full.sh

--- scan-full.sh
+++ scan-full.sh
@@ -14,7 +14,7 @@
 #				
 echo "$(date)" "Processing $1"
 
-nmap -sSU -A --script "(default or safe or ssl-enum-ciphers or http-auth-finder ) and not (http-default-accounts or http-mobileversion-checker or http-comments-displayer or http-slowloris-check or p2p-conficker or qscan or path-mtu or broadcast or external or smb-mbenum or firewalk or reverse-index or url-snarf or http-useragent-tester or http-grep)" -vvv  -PS21,22,23,25,53,80,135,139,389,443,445,636,3389 -pT:-,U:53,69,123,137,161,251,500,523,1434,1900,2065,2067,4500  --host-timeout 20m --version-all --open  --excludefile ./lists/excludes-full.txt -R --webxml -oA "./logs/$1" "$1" > "./logs/$1.txt" 2> "./logs/$1.err"
+nmap -sSU -A --script "(default or safe or ssl-enum-ciphers or http-auth-finder ) and not (http-default-accounts or http-mobileversion-checker or http-comments-displayer or http-slowloris-check or p2p-conficker or qscan or path-mtu or broadcast or external or smb-mbenum or firewalk or reverse-index or url-snarf or http-useragent-tester or http-grep)" -vvv  -PS21,22,23,25,53,80,135,139,389,443,445,636,3389 -pT:-,U:53,69,88,123,137,161,251,389,500,523,1434,1900,2065,2067,4500  --host-timeout 20m --version-all --open  --excludefile ./lists/excludes-full.txt -R --webxml -oA "./logs/$1" "$1" > "./logs/$1.txt" 2> "./logs/$1.err"
 
 
 ./util-checkhost.rb --filename "$1.xml"
